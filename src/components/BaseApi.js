4f347e0c7796aac52b75154605afe82ef05cb58f
MonkeyCode <46069882+rohillavicky319@users.noreply.github.com>
2019-10-11 22:43:00 +0530

    Removed Previous Code
    Removal of Old Code

--- src/components/BaseApi.js
+++
@@ -1,2 +0,0 @@
-const base_url = "http://139.59.73.212:5000/api";
-export default base_url;
--- src/views/Pages/index.js
+++
@@ -1,9 +0,0 @@
-import Login from './Login';
-import Page404 from './Page404';
-import Page500 from './Page500';
-import Register from './Register';
-import Home from './Home';
-
-export {
-  Login, Page404, Page500, Register,Home
-};
