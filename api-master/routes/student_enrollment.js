82a70fff406835bdfd100183a37f12d484499669
MonkeyCode <46069882+rohillavicky319@users.noreply.github.com>
2019-11-01 23:16:51 +0530

    Updated the logo
    Updated the logo

--- api-master/routes/student_enrollment.js
+++ api-master/routes/student_enrollment.js
@@ -98,6 +98,7 @@ router.post("/enroll/new", (req, res) => {
             college_email: req.body.college_email,
             college_contatc_no: req.body.college_contatc_no
           });
+          
 
           newStudent.save()
           .then(student => 
@@ -364,7 +365,7 @@ router.post("/enroll/new", (req, res) => {
                               
                                 <div align="center" class="img-container center  autowidth  " style="padding-right: 0px;  padding-left: 0px;">
             <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px;line-height:0px;"><td style="padding-right: 0px; padding-left: 0px;" align="center"><![endif]-->
-              <img class="center  autowidth " align="center" border="0" src="https://i.imgur.com/FrVRrnL.png" alt="Image" title="Image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: 0;height: auto;float: none;width: 100%;max-width: 250px" width="250">
+              <img class="center  autowidth " align="center" border="0" src="../img/prime_logo2.png" alt="Image" title="Image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: 0;height: auto;float: none;width: 100%;max-width: 250px" width="250">
             <!--[if mso]></td></tr></table><![endif]-->
             </div>
             
