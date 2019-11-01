91473fecef65c71cd355edf7763eae3a44da9dda
MonkeyCode <46069882+rohillavicky319@users.noreply.github.com>
2019-11-01 23:19:47 +0530

    Updated the baseAPI File
    Updated the BaseAPI File

--- job_portal_admin_new-master/src/components/BaseApi.js
+++ job_portal_admin_new-master/src/components/BaseApi.js
@@ -1,21 +1,22 @@
-const url = {
-    "base_url":"http://localhost:5000/api",
-    "home_url":"http://localhost:3000",
-	"download_url":"http://localhost:5000/",
-	"download_url2":"http://localhost:5000",
-    "login_url":"http://localhost:8000",
-    "captcha_key":"6LfhwZkUAAAAAIowLugY-6WbS3WWjhMeI94UaiCG",
-    "grades":["A+","A","A-","B+","B","B-","C+","C","C-","D+","D","D-","E+","E","E-","F"]
-};
+// const url = {
+//     "base_url":"http://localhost:5000/api",
+//     "home_url":"http://localhost:3000",
+// 	"download_url":"http://localhost:5000/",
+// 	"download_url2":"http://localhost:5000",
+//     "login_url":"http://localhost:8000",
+//     "captcha_key":"6LfhwZkUAAAAAIowLugY-6WbS3WWjhMeI94UaiCG",
+//     "grades":["A+","A","A-","B+","B","B-","C+","C","C-","D+","D","D-","E+","E","E-","F"]
+// };
+ const url = {
+        "base_url":"http://primecareers.co.in:5000/api",
+        "home_url":"http://primecareers.co.in:3000",
+        "download_url":"http://primecareers.co.in:5000/",
+        "download_url2":"http://primecareers.co.in:5000",
+        "login_url":"http://primecareers.co.in:3001",
+        "captcha_key":"6LfhwZkUAAAAAIowLugY-6WbS3WWjhMeI94UaiCG",
+        "grades":["A+","A","A-","B+","B","B-","C+","C","C-","D+","D","D-","E+","E","E-","F"]
+    };
 export default url;
 
 
-    // const url = {
-    //     "base_url":"http://primecareers.co.in:5000/api",
-    //     "home_url":"http://primecareers.co.in:3000",
-    //     "download_url":"http://primecareers.co.in:5000/",
-    //     "download_url2":"http://primecareers.co.in:5000",
-    //     "login_url":"http://primecareers.co.in:3001",
-    //     "captcha_key":"6LfhwZkUAAAAAIowLugY-6WbS3WWjhMeI94UaiCG",
-    //     "grades":["A+","A","A-","B+","B","B-","C+","C","C-","D+","D","D-","E+","E","E-","F"]
-    // };
+   
